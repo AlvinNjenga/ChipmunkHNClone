5e533cdab688cdc16abf9fe64bb5afb626a074d5
Alvin N <alvinnjenga@gmail.com>
2019-01-07 17:35:41 +0000

    Updates

---
+++ precache-manifest.b26b232bb876bbd99c092808f73423f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4da54ac11ad0da0d726",
+    "url": "/ChipmunkHNClone/static/js/runtime~main.e4da54ac.js"
+  },
+  {
+    "revision": "2f89d81001bcfee42d67",
+    "url": "/ChipmunkHNClone/static/js/main.2f89d810.chunk.js"
+  },
+  {
+    "revision": "a94dfdf2225e762dbf4a",
+    "url": "/ChipmunkHNClone/static/js/1.a94dfdf2.chunk.js"
+  },
+  {
+    "revision": "2f89d81001bcfee42d67",
+    "url": "/ChipmunkHNClone/static/css/main.4214349e.chunk.css"
+  },
+  {
+    "revision": "748db7c90b9f7643b183a66512db3806",
+    "url": "/ChipmunkHNClone/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ChipmunkHNClone/precache-manifest.7f698eec0f0d2ae92860636ad2202f11.js"
+  "/ChipmunkHNClone/precache-manifest.b26b232bb876bbd99c092808f73423f1.js"
 );
 
 workbox.clientsClaim();
